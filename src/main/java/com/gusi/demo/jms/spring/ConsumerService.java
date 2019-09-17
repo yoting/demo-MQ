3937e92052f6f53aeb1ea40a5036cef9097d4de8
dyy_gusi <dyy_gusi@sina.com>
2019-09-17 17:54:01 +0800

    add ignore

--- src/main/java/com/gusi/demo/jms/spring/ConsumerService.java
+++
@@ -1,7 +0,0 @@
-package com.gusi.demo.jms.spring;
-
-/**
- * Created by gusi on 2017/8/28.
- */
-public interface ConsumerService {
-}
--- src/main/java/com/gusi/demo/jms/spring/ProducerService.java
+++
@@ -1,8 +0,0 @@
-package com.gusi.demo.jms.spring;
-
-/**
- * Created by gusi on 2017/8/28.
- */
-public interface ProducerService {
-    public void sendMessage(String msg);
-}
